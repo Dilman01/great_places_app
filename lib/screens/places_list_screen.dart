bfc15f6f057c89456998e102979e5710c58331df
Dilman Arif <dilmanarif01@gmail.com>
2022-07-21 19:57:01 +0300

    Initial commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Places',
+        ),
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
