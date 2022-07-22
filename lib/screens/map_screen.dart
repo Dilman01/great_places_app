1434261c190281cb7bac422fb86b8ad5c01bba21
Dilman Arif <dilmanarif01@gmail.com>
2022-07-23 01:55:15 +0300

    Allow users to pick a location on the map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -9,20 +9,44 @@ class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
   final bool isSelected;
 
-  MapScreen(
-      {this.initialLocation =
-          const PlaceLocation(latitude: 37.422, longitude: -122.084),
-      this.isSelected = false});
+  MapScreen({
+    this.initialLocation =
+        const PlaceLocation(latitude: 37.422, longitude: -122.084),
+    this.isSelected = false,
+  });
 
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
+  void _selectLocation(LatLng positon) {
+    setState(() {
+      _pickedLocation = positon;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Your Map')),
+      appBar: AppBar(
+        title: Text('Your Map'),
+        actions: [
+          if (widget.isSelected)
+            IconButton(
+              icon: Icon(
+                Icons.check,
+              ),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+            ),
+        ],
+      ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
           target: LatLng(
@@ -31,6 +55,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 16,
         ),
+        onTap: widget.isSelected ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? {}
+            : {
+                Marker(
+                  markerId: MarkerId('m1'),
+                  position: _pickedLocation!,
+                ),
+              },
       ),
     );
   }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:great_places_app/screens/map_screen.dart';
 import 'package:location/location.dart';
 import 'package:great_places_app/helpers/location_helper.dart';
@@ -28,7 +29,7 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(
+    final selectedLocation = await Navigator.of(context).push<LatLng>(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (context) => MapScreen(
@@ -39,6 +40,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
+    print(selectedLocation.latitude);
   }
 
   @override
